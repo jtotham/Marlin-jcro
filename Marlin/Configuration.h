08516ba60902258e45b1f0fecf935faf702fb4bd
jtotham <jon@wirehive.net>
2014-01-11 12:39:58 +0000

    moved z-probe to back middle

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -346,13 +346,13 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define MANUAL_Y_HOME_POS 0
 #define MANUAL_Z_HOME_POS 235  // For delta: Distance between nozzle and print surface after homing.
 
-#define AUTOLEVEL_GRID 24  // Distance between autolevel Z probing points, should be less than print surface radius/3.
+#define AUTOLEVEL_GRID 30  // Distance between autolevel Z probing points, should be less than print surface radius/3.
 
 //// MOVEMENT SETTINGS
 #define NUM_AXIS 4 // The axis order in all axis related arrays is X, Y, Z, E
 #define HOMING_FEEDRATE {100*60, 100*60, 100*60, 0}  // set the homing speeds (mm/min)
 
-#define Z_PROBE_OFFSET {10, 10, -1.9, 0}  // X, Y, Z, E distance between hotend nozzle and deployed bed leveling probe.
+#define Z_PROBE_OFFSET {5, 20, -1.7, 0}  // X, Y, Z, E distance between hotend nozzle and deployed bed leveling probe.
 
 // default settings
 
